bac7cf45ccaa3df5916020e2ed52f24d0ab6b44d
mendess <pedro.mendes.26@gmail.com>
2020-10-12 20:13:25 +0100

    change to pause

--- lab1/lab1.ino
+++ lab1/lab1.ino
@@ -1,16 +1,17 @@
-enum { BUTTON_PIN = 3 };
+enum { BUTTON_PIN = 3, DELAY = 1000 };
 
 typedef enum {
     YELLOW = 4,
     BLUE = 5,
     RED = 6,
     GREEN = 7,
+    OFF = 8,
 } LED;
 
-volatile bool SAVE_LED = LOW;
+volatile bool PAUSE = false;
 
-void save_led() {
-    SAVE_LED = HIGH;
+void toggle_pause() {
+    PAUSE = !PAUSE;
 }
 
 void setup() {
@@ -20,24 +21,23 @@ void setup() {
     pinMode(GREEN, OUTPUT);
     pinMode(LED_BUILTIN, OUTPUT);
     pinMode(BUTTON_PIN, INPUT);
-    attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), save_led, RISING);
+    attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), toggle_pause, FALLING);
 }
 
-LED const leds[] = {RED, GREEN, BLUE, YELLOW};
-#define N_LEDS (sizeof leds / sizeof(int))
+#define N_LEDS ((sizeof leds / sizeof(int)) - 1)
 #define PREV(i) (((i) -1) % N_LEDS)
-#define DELAY 1000
 
 void loop() {
-    SAVE_LED = LOW;
-    for (size_t i = 0; i < N_LEDS; ++i) {
-        digitalWrite(leds[PREV(i)], SAVE_LED);
-        SAVE_LED = LOW;
-        digitalWrite(leds[i], HIGH);
-        delay(DELAY);
+    LED const leds[] = {RED, GREEN, BLUE, YELLOW, OFF};
+    static size_t state = 0;
+    if (PAUSE) {
+        return;
+    } else if (leds[state] == OFF) {
+        for (size_t i = 0; i < N_LEDS; ++i) digitalWrite(leds[i], LOW);
+    } else {
+        digitalWrite(leds[PREV(state)], LOW);
+        digitalWrite(leds[state], HIGH);
     }
-    for (size_t i = 0; i < N_LEDS; ++i) {
-        digitalWrite(leds[i], LOW);
-    }
+    state = (state + 1) % (N_LEDS + 1);
     delay(DELAY);
 }
