f43a9bd24361b7b7420c3275553aa0211c556542
Dominika Florczykowska <dflorczy@protonmail.com>
2020-11-08 19:08:08 +0000

    Add api handling and general program structure

--- lab3/serial_interface/main.py
+++ lab3/serial_interface/main.py
@@ -1,7 +1,26 @@
 import time
-
 import serial
 import logging
+import requests
+
+ROLE = 'sensor' # sensor or actuator
+POTENTIOMETER = 'potentiometer'
+LIGHT = 'light'
+TEMP = 'temp'
+URL = "http://localhost:8000"
+
+
+class Api:
+
+    def __init__(self, url):
+        self.url = url
+
+    def getSensor(self, sensor):
+        response = requests.get(self.url + f'/{sensor}')
+        return response.json()['value']
+
+    def setSensor(self, sensor, value):
+        requests.post(self.url + f'/{sensor}', data={'value': value})
 
 
 class UARTConsole:
@@ -29,16 +48,31 @@ class UARTConsole:
         text_read = self.ser.read(bytes_to_read).decode(encoding='utf-8')
         return text_read
 
+    def write(self, value):
+        # TODO
+        pass
+
 
 def main():
-    rssi_results = []
     try:
         uart = UARTConsole()
-        while True:
-            print(uart.read())
-            time.sleep(1.0)
+        api = Api(URL)
+        if ROLE == 'sensor':
+            while True:
+                analog_read = uart.read()
+                print(analog_read)
+                # TODO: parse and calculate
+                # TODO: make API call
+                time.sleep(1.0)
+        elif ROLE == 'actuator':
+            while True:
+                potentiometer = api.getSensor(POTENTIOMETER)
+                light = api.getSensor(LIGHT)
+                temp = api.getSensor(TEMP)
+                uart.write(potentiometer)
+                uart.write(light)
+                uart.write(temp)
     except KeyboardInterrupt:
-        print(rssi_results)
         exit(1)
 
 
